74cd893b6aa69e953700cb72184a99f050e835d8
Jouse then <jthen93@gmail.com>
2021-02-22 20:29:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self.end_with?(".")
+    return false
   end
 
   def question?
-
+    return true if self.end_with?("?")
+    return false
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+    return false
   end
 
   def count_sentences
-
+    sentence_count = 0 
+    
+    sentence_count = self.split(" ").count do |x|
+      binding.pry 
+      if x.sentence? || x.question? || x.exclamation?
+        return true
+      end
+    end
+   
   end
 end
